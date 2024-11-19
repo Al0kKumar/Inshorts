f13718a9245ff0a77dd353a3ae6e3f1a95314e76
Alok <mishraalok189381@gmail.com>
2024-11-19 19:16:50 +0530

    header fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ const Container = styled(Box)(({ theme }) => ({
 function App() {
 
   return (
-    <Box style={{backgrooundColor: 'black', minHeight: '100vh'}}>
+    <Box >
       <Header />
       <Container>
         <Articles />
